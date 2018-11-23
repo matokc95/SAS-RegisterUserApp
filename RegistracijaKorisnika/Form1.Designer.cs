6aefd698197ec5d55472a7875371d317fb8684b6
ACA_BRUTALNI\Lukas <lukkristi@foi.hr>
2018-11-23 21:40:41 +0100

    Implementirane funkcionalnosti u glavnoj formi za registraciju profesora i validacija unosa

--- RegistracijaKorisnika/Form1.Designer.cs
+++ RegistracijaKorisnika/Form1.Designer.cs
@@ -28,12 +28,160 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.ckbProfesor = new System.Windows.Forms.CheckBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtTitula = new System.Windows.Forms.TextBox();
+            this.txtEmail = new System.Windows.Forms.TextBox();
+            this.txtPrezime = new System.Windows.Forms.TextBox();
+            this.txtIme = new System.Windows.Forms.TextBox();
+            this.txtOdgovor = new System.Windows.Forms.TextBox();
+            this.btnRegistracija = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // ckbProfesor
+            // 
+            this.ckbProfesor.AutoSize = true;
+            this.ckbProfesor.Location = new System.Drawing.Point(168, 200);
+            this.ckbProfesor.Margin = new System.Windows.Forms.Padding(2);
+            this.ckbProfesor.Name = "ckbProfesor";
+            this.ckbProfesor.Size = new System.Drawing.Size(65, 17);
+            this.ckbProfesor.TabIndex = 21;
+            this.ckbProfesor.Text = "Profesor";
+            this.ckbProfesor.UseVisualStyleBackColor = true;
+            this.ckbProfesor.CheckStateChanged += new System.EventHandler(this.ckbProfesor_CheckStateChanged);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(89, 170);
+            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(33, 13);
+            this.label4.TabIndex = 20;
+            this.label4.Text = "Titula";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(89, 131);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(32, 13);
+            this.label3.TabIndex = 19;
+            this.label3.Text = "Email";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(89, 92);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(44, 13);
+            this.label2.TabIndex = 18;
+            this.label2.Text = "Prezime";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(89, 53);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(24, 13);
+            this.label1.TabIndex = 17;
+            this.label1.Text = "Ime";
+            // 
+            // txtTitula
+            // 
+            this.txtTitula.Enabled = false;
+            this.txtTitula.Location = new System.Drawing.Point(168, 167);
+            this.txtTitula.Margin = new System.Windows.Forms.Padding(2);
+            this.txtTitula.Name = "txtTitula";
+            this.txtTitula.Size = new System.Drawing.Size(142, 20);
+            this.txtTitula.TabIndex = 16;
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.Location = new System.Drawing.Point(168, 128);
+            this.txtEmail.Margin = new System.Windows.Forms.Padding(2);
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(142, 20);
+            this.txtEmail.TabIndex = 15;
+            // 
+            // txtPrezime
+            // 
+            this.txtPrezime.Location = new System.Drawing.Point(168, 89);
+            this.txtPrezime.Margin = new System.Windows.Forms.Padding(2);
+            this.txtPrezime.Name = "txtPrezime";
+            this.txtPrezime.Size = new System.Drawing.Size(142, 20);
+            this.txtPrezime.TabIndex = 14;
+            // 
+            // txtIme
+            // 
+            this.txtIme.Location = new System.Drawing.Point(168, 50);
+            this.txtIme.Margin = new System.Windows.Forms.Padding(2);
+            this.txtIme.Name = "txtIme";
+            this.txtIme.Size = new System.Drawing.Size(142, 20);
+            this.txtIme.TabIndex = 13;
+            // 
+            // txtOdgovor
+            // 
+            this.txtOdgovor.Location = new System.Drawing.Point(124, 320);
+            this.txtOdgovor.Margin = new System.Windows.Forms.Padding(2);
+            this.txtOdgovor.Multiline = true;
+            this.txtOdgovor.Name = "txtOdgovor";
+            this.txtOdgovor.Size = new System.Drawing.Size(218, 90);
+            this.txtOdgovor.TabIndex = 12;
+            // 
+            // btnRegistracija
+            // 
+            this.btnRegistracija.Location = new System.Drawing.Point(168, 241);
+            this.btnRegistracija.Margin = new System.Windows.Forms.Padding(2);
+            this.btnRegistracija.Name = "btnRegistracija";
+            this.btnRegistracija.Size = new System.Drawing.Size(141, 38);
+            this.btnRegistracija.TabIndex = 11;
+            this.btnRegistracija.Text = "Registriraj korisnika";
+            this.btnRegistracija.UseVisualStyleBackColor = true;
+            this.btnRegistracija.Click += new System.EventHandler(this.btnRegistracija_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(461, 482);
+            this.Controls.Add(this.ckbProfesor);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtTitula);
+            this.Controls.Add(this.txtEmail);
+            this.Controls.Add(this.txtPrezime);
+            this.Controls.Add(this.txtIme);
+            this.Controls.Add(this.txtOdgovor);
+            this.Controls.Add(this.btnRegistracija);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.CheckBox ckbProfesor;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtTitula;
+        private System.Windows.Forms.TextBox txtEmail;
+        private System.Windows.Forms.TextBox txtPrezime;
+        private System.Windows.Forms.TextBox txtIme;
+        private System.Windows.Forms.TextBox txtOdgovor;
+        private System.Windows.Forms.Button btnRegistracija;
     }
 }
 
